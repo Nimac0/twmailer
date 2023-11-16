50b5af8c341756c60752b260cbc11d93da673763
Aisa <aisaemese@gmail.com>
2023-11-16 21:51:08 +0100

    compared server to sample, bit of formatting

--- server.cpp
+++ server.cpp
@@ -4,6 +4,7 @@ namespace fs = std::filesystem;
 
 #define BUF 1024
 #define PORT 6543
+#define BACKLOG 10 // Count of waiting connections allowed
 
 #define LDAP_LOGIN_FAILED -2
 #define LDAP_LOGIN_ERROR -1
@@ -23,49 +24,38 @@ int main(int argc, char** argv)
 //--------------SET UP SOCKET CONNECTION-------------
     socklen_t addrlen;
     struct sockaddr_in address, cliaddress;
-    int reuseValue = 1; // bool
+    int reuseValue = 1;
 
-    if (signal(SIGINT, signalHandler) == SIG_ERR)
-    {
-        perror("signal can not be registered");
+    if (signal(SIGINT, signalHandler) == SIG_ERR) {
+        perror("Signal can not be registered");
         return EXIT_FAILURE;
     }
-    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
+    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket error");
         return EXIT_FAILURE;
     }
-    if (setsockopt(create_socket,
-                    SOL_SOCKET,
-                    SO_REUSEADDR,
-                    &reuseValue,
-                    sizeof(reuseValue)) == -1)
-    {
-        perror("set socket options - reuseAddr");
+    if (setsockopt(create_socket, SOL_SOCKET, SO_REUSEADDR, &reuseValue, sizeof(reuseValue)) == -1) {
+        perror("Set socket options - reuseAddr");
         return EXIT_FAILURE;
     }
-    if (setsockopt(create_socket,
-                    SOL_SOCKET,
-                    SO_REUSEPORT,
-                    &reuseValue,
-                    sizeof(reuseValue)) == -1)
-    {
+    if (setsockopt(create_socket, SOL_SOCKET, SO_REUSEPORT, &reuseValue, sizeof(reuseValue)) == -1) {
         perror("set socket options - reusePort");
         return EXIT_FAILURE;
     }
+
+    // TODO: User Defined PORT
+
+    // Initiate Address
     memset(&address, 0, sizeof(address));
-    
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     
-    if (bind(create_socket, (struct sockaddr *)&address, sizeof(address)) == -1)
-    {
+    if (bind(create_socket, (struct sockaddr *)&address, sizeof(address)) == -1) {
         perror("bind error");
         return EXIT_FAILURE;
     }
-    if (listen(create_socket, 5) == -1)
-    {
+    if (listen(create_socket, BACKLOG) == -1) {
         perror("listen error");
         return EXIT_FAILURE;
     }
@@ -74,41 +64,29 @@ int main(int argc, char** argv)
     while(!abortRequested)
     {
         printf("Waiting for connections...\n");
+
         addrlen = sizeof(struct sockaddr_in);
-        // Accept new client
-        if ((new_socket = accept(create_socket,
-                                (struct sockaddr *)&cliaddress,
-                                &addrlen)) == -1)
-        {
-            if (abortRequested)
-            {
-                perror("accept error after aborted");
-            }
-            else
-            {
-                perror("accept error");
+        if ((new_socket = accept(create_socket, (struct sockaddr *)&cliaddress, &addrlen)) == -1) {
+            if (abortRequested) {
+                perror("Accept error after aborted");
+            } else {
+                perror("accept error"); 
             }
             break;
         }
-        printf("Client connected from %s:%d...\n",
-        inet_ntoa(cliaddress.sin_addr),
-        ntohs(cliaddress.sin_port));
 
+        printf("Client connected from %s:%d...\n", inet_ntoa(cliaddress.sin_addr), ntohs(cliaddress.sin_port));
+        // Used for limiting Log in attempts
         createBlacklist();
-        std::string clientIP(inet_ntoa(cliaddress.sin_addr));
-
-        clientCommunication(&new_socket, clientIP);
-
+        clientCommunication(&new_socket, std::string(inet_ntoa(cliaddress.sin_addr)));
         new_socket = -1;
     }
-    if (create_socket != -1)
-    {
-        if (shutdown(create_socket, SHUT_RDWR) == -1)
-        {
+    // Free descriptor
+    if (create_socket != -1) {
+        if (shutdown(create_socket, SHUT_RDWR) == -1) {
             perror("shutdown create_socket");
         }
-        if (close(create_socket) == -1)
-        {
+        if (close(create_socket) == -1) {
             perror("close create_socket");
         }
         create_socket = -1;
@@ -123,64 +101,56 @@ void *clientCommunication(void *data, const std::string clientIP)
     int *current_socket = (int *)data;
 
     unsigned int loginAttempts = 0;
+    // TODO: Not in the original...
+    memset(buffer, 0, sizeof(buffer));
 
     do
     {   
-        memset(buffer, 0, sizeof buffer);
         size = recv(*current_socket, buffer, BUF - 1, 0);
-//-----------------ABORT AND ERROR HANDLING-----------------
-        if (size == -1)
-        {
-            if (abortRequested)
-            {
-                perror("recv error after aborted");
-            }
-            else
-            {
-                perror("recv error");
+    //-----------------ABORT AND ERROR HANDLING-----------------
+        if (size == -1) {
+            if (abortRequested) {
+                perror("Receive error after aborted");
+            } else {
+                perror("Receive error");
             }
             break;
         }
-
-        if (size == 0)
-        {
+        if (size == 0) {
             printf("Client closed remote socket\n");
             break;
         }
-//----------------------------------------------------------
+    //----------------------------------------------------------
         trimEnd(&buffer[0], &size);
-        printf("Message received:\n%s\n", buffer);
         std::string message(buffer);
+        std::cout << "Message received:\n" + message + "\n";
 
         if (commandFound(message, "LOGIN")) 
         {
+            // TODO: Disable any recieving until this period is over
             // Check if user is allowed to log in
-            if (userBlacklisted(clientIP))
-            {
-                if (send(*current_socket, "ERR. Please wait one minute before you try again\n", 49, 0) == -1) 
-                {
+            if (userBlacklisted(clientIP)) {
+                if (send(*current_socket, "ERR. Please wait one minute before you try again\n", 49, 0) == -1) {
                     std::cerr << "Failed to send answer\n";
                     return NULL;
                 }
             }
             // Log user in
             int returnCode = handleLogin(message);
-            if (returnCode == LDAP_LOGIN_FAILED)
-            {
+            if (returnCode == LDAP_LOGIN_FAILED) {
                 loginAttempts++;
                 std::cerr << "N. of Attempts: " << loginAttempts << "\n";
-                if (loginAttempts >= 3)
-                {
+                // Blacklist user if 3 attempts exceeded
+                if (loginAttempts >= 3) {
                     std::cerr << "In three attempts if\n";
-                    if (!manageBlacklist(clientIP))
-                    {
+                    if (!manageBlacklist(clientIP)) {
                         std::cerr << "Error removing user from blacklist\n";
                     }
                     loginAttempts = 0;
                 }
             }
-            else if (returnCode == LDAP_LOGIN_SUCCESS)
-            {
+            // Successful login
+            else if (returnCode == LDAP_LOGIN_SUCCESS) {
                 if (send(*current_socket, "OK\n", 3, 0) == -1) 
                 {
                     std::cerr << "Failed to send answer\n";
@@ -249,6 +219,7 @@ void *clientCommunication(void *data, const std::string clientIP)
             }
         }
     } while (strcmp(buffer, "QUIT") != 0 && !abortRequested);
+    
 // Shut down and close socket connection
     if (*current_socket != -1)
     {
